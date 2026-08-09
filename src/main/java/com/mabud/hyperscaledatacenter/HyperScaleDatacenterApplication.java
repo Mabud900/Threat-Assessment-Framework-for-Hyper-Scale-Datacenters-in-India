9f769d43810ed933f1a51893f31d0c8d90dfad40
md mabud <mdmabud.mail56@gmail.com>
2026-08-09 11:27:14 +0530

    Project Setup

---
+++ src/main/java/com/mabud/hyperscaledatacenter/HyperScaleDatacenterApplication.java
@@ -0,0 +1,14 @@
+package com.mabud.hyperscaledatacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperScaleDatacenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperScaleDatacenterApplication.class, args);
+        System.out.println("Spring Boot Running..");
+    }
+
+}
---
+++ src/test/java/com/mabud/hyperscaledatacenter/HyperScaleDatacenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabud.hyperscaledatacenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperScaleDatacenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

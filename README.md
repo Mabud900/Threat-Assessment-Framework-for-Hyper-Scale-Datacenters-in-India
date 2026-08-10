db6bf21bd31d9a08365c2493c6045e914ad42792
md mabud <mdmabud.mail56@gmail.com>
2026-08-10 22:19:21 +0530

    Added Entity and Repository

---
+++ README.md
@@ -0,0 +1,1 @@
+# Threat Assessment Framework for Hyper Scale Datacenters in India
--- src/main/java/com/mabud/hyperscaledatacenter/HyperScaleDatacenterApplication.java
+++ src/main/java/com/mabud/hyperscaledatacenter/HyperScaleDatacenterApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HyperScaleDatacenterApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(HyperScaleDatacenterApplication.class, args);
         System.out.println("Spring Boot Running..");
